4f9120d8eff758664e7e8ae74b107e6a7f388c99
Andrew ZONG <a.zong@f5.com>
2021-05-01 12:51:34 +0800

    simple periodic work.

--- src/ngx_http_periodic_task_module.c
+++ src/ngx_http_periodic_task_module.c
@@ -1,12 +1,16 @@
 #include <nginx.h>
 #include "ngx_http_periodic_task_module.h"
 
+static ngx_event_t periodic_task_event;
+static ngx_int_t periodic_interval = 1;
+
 static ngx_int_t
 ngx_http_periodic_task_init_process(ngx_cycle_t *cycle);
 
 static ngx_command_t ngx_http_periodic_task_commands[] = {
     ngx_null_command
 };
+static void myevent_callback(ngx_event_t *ev);
 
 static ngx_http_module_t  ngx_http_periodic_task_module_ctx = {
     NULL,                                    /* preconfiguration */
@@ -40,15 +44,28 @@ ngx_module_t  ngx_http_periodic_task_module = {
 static ngx_int_t
 ngx_http_periodic_task_init_process(ngx_cycle_t *cycle)
 {
-    
-    for(int i=0; i<10; i++) {
-        ngx_log_error(NGX_ERROR_INFO, cycle->log, i, 
-            "ngx_http_periodic_task_init_process Called here");
-    }
+    ngx_log_error(NGX_ERROR_INFO, cycle->log, 0, 
+        "zongzw ngx_http_periodic_task_init_process Called here");
     
     if (ngx_process != NGX_PROCESS_WORKER) {
         return NGX_OK;
     }
 
+    ngx_memzero(&periodic_task_event, sizeof(ngx_event_t));
+
+    periodic_task_event.handler = myevent_callback;
+    periodic_task_event.log = cycle->log;
+
+    ngx_add_timer(&periodic_task_event, periodic_interval);
     return NGX_OK;
+}
+
+static void
+myevent_callback(ngx_event_t *ev) 
+{
+    ngx_log_error(NGX_ERROR_INFO, ev->log, 0,
+        "zongzw timer callback is triggered");
+
+    periodic_interval *= 2;
+    ngx_add_timer(ev, periodic_interval);
 }
