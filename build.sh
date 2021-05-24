4b7448679dee77b949d108c2a3c31565897b5cb4
Andrew ZONG <a.zong@f5.com>
2021-05-24 09:58:39 +0800

    ngx_http_periodic_module use cycle allocation instead of global variable.

--- build.sh
+++ build.sh
@@ -9,12 +9,12 @@ nginx-build \
     -d $cdir \
     --add-module=$cdir/src/ngx_http_variable_module \
     --add-module=$cdir/src/ngx_http_content_module \
-    --add-module=$cdir/src/ngx_core_periodic_module \
+   --add-module=$cdir/src/ngx_http_periodic_module \
     --with-debug \
     --with-threads \
     --prefix=$cdir/execs
 
-#    --add-module=$cdir/src/ngx_http_periodic_module \
+#     --add-module=$cdir/src/ngx_core_periodic_module \
 (
     cd $cdir/nginx/$nginx_version/nginx-$nginx_version
     make install
--- src/ngx_http_periodic_module/src/ngx_http_periodic_module.c
+++ src/ngx_http_periodic_module/src/ngx_http_periodic_module.c
@@ -1,9 +1,6 @@
 #include <nginx.h>
 #include "ngx_http_periodic_module.h"
 
-// TODO: 是否可以删除此全局变量，将其放置在cycle->pool中。
-static ngx_event_t periodic_event;
-
 static ngx_int_t
 ngx_http_periodic_init_process(ngx_cycle_t *cycle);
 
@@ -12,6 +9,7 @@ ngx_http_periodic_exit_process(ngx_cycle_t *cycle);
 
 typedef struct {
     ngx_int_t periodic_interval;
+    ngx_event_t ev;
 } ngx_http_periodic_conf_t;
 
 static char*
@@ -92,8 +90,8 @@ ngx_http_set_periodic_interval(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     p->periodic_interval = ngx_atoi(value[1].data, value[1].len);
 
     ngx_log_error(NGX_ERROR_INFO, cf->log, 0, 
-        "%s: directive %V set value %i", 
-        __FUNCTION__, &value[0], p->periodic_interval);
+        "%s: directive %V set value %i, conf: %p", 
+        __FUNCTION__, &value[0], p->periodic_interval, p);
 
     return NGX_CONF_OK;
 }
@@ -102,14 +100,13 @@ char*
 ngx_http_periodic_init_main_conf(ngx_conf_t *cf, void *conf)
 {
     ngx_http_periodic_conf_t *pcf = conf;
-    ngx_memzero(&periodic_event, sizeof(ngx_event_t));
-    
-    periodic_event.handler = periodic_callback;
-    periodic_event.data = conf;
+
+    pcf->ev.handler = periodic_callback;
+    pcf->ev.data = conf;
 
     ngx_log_error(NGX_ERROR_INFO, cf->log, 0, 
-        "%s: periodic_interval: %i", 
-        __FUNCTION__, pcf->periodic_interval);
+        "%s: periodic_interval: %i, conf: %p", 
+        __FUNCTION__, pcf->periodic_interval, pcf);
 
     return NGX_CONF_OK;
 }
@@ -117,32 +114,27 @@ ngx_http_periodic_init_main_conf(ngx_conf_t *cf, void *conf)
 static ngx_int_t
 ngx_http_periodic_init_process(ngx_cycle_t *cycle)
 {
-    periodic_event.log = cycle->log;
-    ngx_http_periodic_conf_t *pcf = periodic_event.data;
+
+    ngx_http_periodic_conf_t *cpcf;
+    cpcf = (ngx_http_periodic_conf_t*)ngx_http_cycle_get_module_main_conf(
+        cycle, ngx_http_periodic_module);
+
+    ngx_log_error(NGX_ERROR_INFO, cycle->log, 0, "%s cpcf: %p", __FUNCTION__, cpcf);
+
+    cpcf->ev.log = cycle->log;
 
     // The way to get ngx_core_conf_t.
     // ngx_core_conf_t  *ccf;
     // ccf = (ngx_core_conf_t *) ngx_get_conf(ngx_cycle->conf_ctx,
     //                                             ngx_core_module);
-    // TODO:
-    // 此处使用cycle->pool重新申请空间保存*运行时* periodic_event的数据信息。
-    // 继续使用ngx_http_periodic_init_main_conf 中的void *conf 会有异常。
-    // 即，在申请空间之前，periodic_event.data起到了一个缓存指针的作用.
-    // 是否可以通过cycle中直接获取配置信息？
-    periodic_event.data = ngx_pcalloc(
-        cycle->pool, sizeof(ngx_http_periodic_conf_t));
-    if (periodic_event.data == NULL) {
-        return NGX_ERROR;
-    }
-    ngx_memcpy(periodic_event.data, pcf, sizeof(ngx_http_periodic_conf_t));
 
-    // if (pcf->periodic_interval > 0) {
-        ngx_add_timer(&periodic_event, pcf->periodic_interval);
-    // }
+    if (cpcf->periodic_interval > 0) {
+        ngx_add_timer(&cpcf->ev, cpcf->periodic_interval);
+    }
 
     ngx_log_error(NGX_ERROR_INFO, cycle->log, 0, 
         "%s: New timer event is added, with interval: %i", 
-        __FUNCTION__, pcf->periodic_interval);
+        __FUNCTION__, cpcf->periodic_interval);
 
     return NGX_OK;
 }
@@ -150,11 +142,11 @@ ngx_http_periodic_init_process(ngx_cycle_t *cycle)
 static void
 ngx_http_periodic_exit_process(ngx_cycle_t *cycle)
 {
-    // TODO:
-    // 因为采用global variable的方式，所以这里不能既清空又设置。
-    // 只有放在cycle中
-    // ngx_http_periodic_conf_t *pcf = periodic_event.data;
-    // pcf->periodic_interval = 0;
+    // ngx_http_periodic_conf_t *cpcf;
+    // cpcf = (ngx_http_periodic_conf_t*)ngx_http_cycle_get_module_main_conf(
+    //     cycle, ngx_http_periodic_module);
+
+    // cpcf->periodic_interval = 0;
 }
 
 static void
@@ -163,9 +155,9 @@ periodic_callback(ngx_event_t *ev)
     ngx_http_periodic_conf_t *pcf; 
 
     pcf = ev->data;
-    // if (pcf->periodic_interval > 0) {
-    //     ngx_add_timer(ev, pcf->periodic_interval);
-    // }
+    if (pcf->periodic_interval > 0) {
+        ngx_add_timer(ev, pcf->periodic_interval);
+    }
 
     ngx_log_error(NGX_ERROR_INFO, ev->log, 0,
         "%s: Periodic task is triggered: %i", __FUNCTION__, pcf->periodic_interval);
